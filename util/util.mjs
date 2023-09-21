b2e22bb9225e7df91e000c893ccaf33167933005
Ortiz25 <samueldeya@gmail.com>
2023-09-21 13:25:44 +0300

    updated recovery sms

--- util/util.mjs
+++ util/util.mjs
@@ -255,7 +255,7 @@ export function QueryBundleBalance(user, res) {
   const query = `SELECT acctinputoctets, acctoutputoctets FROM radacct
   WHERE username =  ?
   GROUP BY username
-  ORDER BY acctupdatetime ASC
+  ORDER BY acctstarttime ASC
   LIMIT 1;`;
 
   db.query(query, [user], async (queryError, results) => {
