e86b28bea880987f412a4b7ee590b8340e9cf8fe
Ortiz25 <samueldeya@gmail.com>
2023-09-29 23:44:43 +0300

    updates app cors

--- app.mjs
+++ app.mjs
@@ -37,16 +37,12 @@ const port = 3000;
 const hostname = "0.0.0.0";
 
 const corsOptions = {
-  origin: [
-    "https://hotspot-frontend-app-5f616.firebaseapp.com",
-    "http://localhost:3000",
-    "http://108.181.203.124:3000",
-    "https://www.livecrib.rent/",
-  ],
-  optionsSuccessStatus: 200,
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
 };
 
-app.use(cors());
+app.use(cors(corsOptions));
 // app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // app.use((req, res, next) => {
