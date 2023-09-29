c742375050aa744b5a7c662f8dfe2ff5c433f0da
Ortiz25 <samueldeya@gmail.com>
2023-09-29 23:31:35 +0300

    updates app auth route

--- app.mjs
+++ app.mjs
@@ -47,8 +47,8 @@ const corsOptions = {
 };
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-const jsonParser = bodyParser.json();
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 // app.use((req, res, next) => {
 //   res.setHeader("Access-Control-Allow-Origin", "*");
 //   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
