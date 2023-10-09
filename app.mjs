b2650432fc1bee2f77ae531b147dad129816a249
Ortiz25 <samueldeya@gmail.com>
2023-10-09 15:36:01 +0300

    first commit update2

--- app.mjs
+++ app.mjs
@@ -36,25 +36,19 @@ const app = express();
 const port = 3000;
 const hostname = "0.0.0.0";
 
-const corsOptions = {
-  origin: "*",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 // app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "Content-Type",
-//     "Authorization"
-//   );
-//   next();
-// });
+app.use((req, res, next) => {
+  req.setHeader("Access-Control-Allow-Origin", "*");
+  req.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  req.setHeader(
+    "Access-Control-Allow-Methods",
+    "Content-Type",
+    "Authorization"
+  );
+  next();
+});
 
 //////////////////TWILIO//////////////////////
 const client = twilio(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
